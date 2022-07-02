9247004bf5ac6c6f2a88b6d4e4fb81cadd755386
Shinchan-git <gyoshu.bowie@gmail.com>
2022-07-02 22:51:28 +0900

    test: next-optimized-images

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = withImageLoader({
   reactStrictMode: true,
   images: {
     loader: 'custom',
+    disableStaticImages: true,
   },
   env: {
     NEXT_PUBLIC_IMAGE_DOMAIN: 'https://yonbiten2022.pages.dev'
