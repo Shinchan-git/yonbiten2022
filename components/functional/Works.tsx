a411388841049c754c25b22525c113d1238f22dc
Shinchan-git <gyoshu.bowie@gmail.com>
2022-07-09 20:59:44 +0900

    add: text underline;

--- components/functional/Works.tsx
+++ components/functional/Works.tsx
@@ -4,6 +4,7 @@ import Spacer from '../ui/Spacer'
 import PageTitle from '../ui/PageTitle'
 import TextContainer from '../ui/TextContainer'
 import TextLink from '../ui/TextLink'
+import { primaryColorAsBackground } from '../../styles/colors'
 
 const Works: React.FC = () => {
   return (
@@ -13,17 +14,17 @@ const Works: React.FC = () => {
         <li>
           <p css={titleStyle}>東大美術サークル</p>
         </li>
-        <Spacer size={18} />
+        <Spacer size={30} />
         <li>
           <p css={titleStyle}>慶應大学 パレットクラブ</p>
-          <p>--作品の画像--</p>
+          <p>--作品の画像など--</p>
           <p>Twitter: <TextLink href="https://twitter.com/palette_KEIO">@palette_KEIO</TextLink></p>
         </li>
-        <Spacer size={18} />
+        <Spacer size={30} />
         <li>
           <p css={titleStyle}>東京女子大学 洋画研究会</p>
         </li>
-        <Spacer size={18} />
+        <Spacer size={30} />
         <li>
           <p css={titleStyle}>早稲田大学 絵画会</p>
         </li>
@@ -33,8 +34,10 @@ const Works: React.FC = () => {
 }
 
 const titleStyle = css`
+  display: inline-block;
   font-size: 22px;
   margin: 8px 0;
+  background:linear-gradient(transparent 80%, ${primaryColorAsBackground} 80%);
 `
 
 export default Works
